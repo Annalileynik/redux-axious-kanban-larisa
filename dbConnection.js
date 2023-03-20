f4b5e93473148cf9269aaf2984872016cf1138cc
annadavydova <lileynik@yahoo.com>
2023-03-20 10:45:09 -0400

    update

---
+++ dbConnection.js
@@ -0,0 +1,9 @@
+// import mongoose from "mongoose";
+//
+// export function dbConnect() {
+//     mongoose.connect('mongodb+srv://anna:AQOPUnhCOmyPzNNu@server-testing.spbcghj.mongodb.net/test',
+//         {userNewUrlParser: true})
+//             .then(() => console.log("Connected!"))
+//             .catch((err) => console.log(err))
+// }
+// mongoose.set("strictQuery", false)
